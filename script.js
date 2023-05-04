5bd31a745556a401c7181823dd96b467e1022540
corey duke <cdukedev@gmail.com>
2023-05-04 07:39:22 -0400

    Created a program to take in prompt completion pairs

---
+++ script.js
@@ -0,0 +1,96 @@
+let filename = "";
+function setCurrentFile() {
+  const fileSelector = document.getElementById("file-selector");
+  const selectedFile = fileSelector.value;
+  localStorage.setItem("currentFilename", selectedFile);
+  alert(`Current file set to '${selectedFile}'.`);
+}
+async function updateFileList() {
+  try {
+    const response = await axios.get("http://localhost:3000/files");
+    const files = response.data;
+
+    const fileSelector = document.getElementById("file-selector");
+    fileSelector.innerHTML = "";
+
+    for (const file of files) {
+      const option = document.createElement("option");
+      option.value = file;
+      option.text = file;
+      fileSelector.appendChild(option);
+    }
+    // Get the current filename from localStorage and set it as the selected value
+    const currentFilename = localStorage.getItem("currentFilename");
+    if (currentFilename) {
+      fileSelector.value = currentFilename;
+    }
+  } catch (error) {
+    console.error(error);
+    alert("Error updating file list.");
+  }
+  return false;
+}
+
+function createFile() {
+  filename = document.getElementById("filename").value;
+  axios
+    .post("http://localhost:3000/create", { filename })
+    .then((response) => {
+      console.log(response.data);
+      alert(`File '${filename}.jsonl' created.`);
+    })
+    .catch((error) => {
+      console.error(error);
+      alert("Error creating file.");
+    });
+  return false;
+}
+
+function addLine() {
+  // Use the selected file from the dropdown menu
+  const fileSelector = document.getElementById("file-selector");
+  filename = fileSelector.value;
+  const prompt = document.getElementById("prompt").value;
+  const completion = document.getElementById("completion").value;
+  const data = { filename, prompt, completion };
+  axios
+    .post("http://localhost:3000/addLine", data)
+    .then((response) => {
+      console.log(response.data);
+
+      // Save the current selected file before clearing the input fields
+      const selectedFile = fileSelector.value;
+
+      // Clear the input fields
+      document.getElementById("prompt").value = "";
+      document.getElementById("completion").value = "";
+
+      // Set the dropdown menu value back to the saved value
+      console.log(selectedFile);
+      fileSelector.value = selectedFile;
+    })
+    .catch((error) => {
+      console.error(error);
+      alert("Error adding line to file.");
+    });
+  return false;
+}
+
+async function saveFile() {
+  try {
+    // Get the value of the selected option in the dropdown
+    const fileSelector = document.getElementById("file-selector");
+    const selectedFilename = fileSelector.value;
+
+    const response = await axios.post("http://localhost:3000/addToCollection", {
+      filename: selectedFilename,
+    });
+    console.log(response.data);
+    alert(`File '${selectedFilename}' saved and added to collection.`);
+  } catch (error) {
+    console.error(error);
+    alert("Error saving file to collection.");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", updateFileList);
---
+++ server/index.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
+const filesDir = path.join(__dirname, "files");
+if (!fs.existsSync(filesDir)) {
+  fs.mkdirSync(filesDir);
+}
+//use cors to allow cross origin resource sharing
+
+const app = express();
+app.use(
+  cors({
+    origin: "*", // or replace '*' with the specific origin you want to allow, e.g., 'http://localhost:8080'
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+// allow any browser to access the server
+app.use(express.json());
+// POST endpoint for creating a new JSONL file
+
+// GET endpoint for retrieving the list of files
+app.get("/files", (req, res) => {
+  fs.readdir(filesDir, (err, files) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error retrieving files");
+    } else {
+      res.send(files);
+    }
+  });
+});
+
+app.post("/create", (req, res) => {
+  try {
+    const { filename } = req.body;
+    const fileContent = "";
+    const filePath = path.join(__dirname, "files", `${filename}.jsonl`);
+
+    fs.writeFile(filePath, fileContent, (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error creating file");
+      } else {
+        console.log(`File '${filename}.jsonl' created.`);
+        res.send(`File '${filename}.jsonl' created.`);
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error in /create endpoint");
+  }
+});
+
+// POST endpoint for adding a line to an existing JSONL file
+app.post("/addLine", (req, res) => {
+  const { filename, prompt, completion } = req.body;
+  const filePath = path.join(__dirname, "files", `${filename}`);
+  const fileContent = `{"prompt": "${prompt}? \\n\\n###\\n\\n", "completion": " ${completion}. ###\\n"}`;
+
+  fs.appendFile(filePath, fileContent + "\n", (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error adding line to file");
+    } else {
+      console.log(`Prompt and completion added to '${filename}'.`);
+      res.send(`Prompt and completion added to '${filename}'.`);
+    }
+  });
+});
+
+// POST endpoint for adding a JSONL file to the collection
+app.post("/addToCollection", (req, res) => {
+  const { filename } = req.body;
+  const filePath = path.join(__dirname, "files", `${filename}`);
+  console.log(filePath);
+  // Check if the file exists
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(404).send(`File '${filename}' not found.`);
+      return;
+    }
+
+    const collectionPath = path.join(__dirname, "data", "collection.jsonl");
+    const readStream = fs.createReadStream(filePath);
+    const writeStream = fs.createWriteStream(collectionPath, { flags: "a" });
+    readStream.pipe(writeStream);
+
+    readStream.on("close", () => {
+      console.log(`File '${filename}' added to collection.`);
+      res.send(`File '${filename}' added to collection.`);
+    });
+
+    readStream.on("error", (error) => {
+      console.error(error);
+      res.status(500).send("Error adding file to the collection.");
+    });
+  });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
